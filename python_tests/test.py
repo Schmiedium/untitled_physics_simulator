d73477245fbbefc02f0a4f8a48050520cabb9461
Alex Eisenschmied <alexander.eisenschmied@gmail.com>
2023-01-02 11:43:24 -0500

    documented python script

--- python_tests/test.py
+++ python_tests/test.py
@@ -2,8 +2,11 @@ import untitled_physics_simulator as ps
 import pyarrow
 import polars as pl
 
-
+#create simulation with 0.001 seconds per timestep, and a sim duration of 5.0 seconds. ~5000 steps
 sim = ps.Simulation(0.001, 5.0)
+
+#create entities and add to the sim
+#you will have to supply your own objs, be carefuul they don't overlap on spawn
 sim.create_entity(index = 3, name = "test1", entity_type = "Dynamic", position = (10.0, 10.0, 0.0), velocity = (0.0, 0.0, 0.0), geometry = "/home/alex/Documents/3D_Geometry/OBJs/icosahedron.obj")
 sim.create_entity(index = 4, name = "test2", entity_type = "Dynamic", position = (10.0, 10.0, 10.0), velocity = (0.0, 0.0, 0.0), geometry = "/home/alex/Documents/3D_Geometry/OBJs/icosahedron.obj")
 sim.create_entity(index = 5, name = "test3", entity_type = "Dynamic", position = (10.0, 10.0, -10.0), velocity = (0.0, 0.0, 0.0), geometry = "/home/alex/Documents/3D_Geometry/OBJs/icosahedron.obj")
@@ -17,12 +20,19 @@ sim.create_entity(index = 12, name = "test10", entity_type = "Dynamic", position
 sim.create_entity(index = 13, name = "test11", entity_type = "Dynamic", position = (7.0, 10.0, -10.0), velocity = (0.0, 0.0, 0.0), geometry = "/home/alex/Documents/3D_Geometry/OBJs/icosahedron.obj")
 sim.create_entity(index = 14, name = "test12", entity_type = "Dynamic", position = (0.0, 20.0, -10.0), velocity = (0.0, 0.0, 0.0), geometry = "/home/alex/Documents/3D_Geometry/OBJs/icosahedron.obj")
 
+
+#run the simulation with a render, render can be turned off by using simulation_run_headless
+#store the output data in a variable
 result = ps.simulation_run(sim)
 
-
+#print the output
 print(result)
-# result['Ball'].write_csv("Ball.csv")
-# result['Ball'].write_parquet("Ball.parquet")
+
+#Optionally write dataframes to csv or parquet
+#These are polars dataframes, so you an do whatever with them
+
+# result['test12'].write_csv("test12.csv")
+# result['test7'].write_parquet("test7.parquet")
 
 
 
